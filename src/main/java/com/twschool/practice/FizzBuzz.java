fceabeec660e87a32567921c735c774300622ebb
liyaofeng <932550363@qq.com>
2020-06-18 14:49:56 +0800

    [liyaofeng] git仓库初始化且完成1的断言测试

---
+++ src/main/java/com/twschool/practice/FizzBuzz.java
@@ -0,0 +1,7 @@
+package com.twschool.practice;
+
+public class FizzBuzz {
+    public String say(Integer number){
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/com/twschool/practice/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+package com.twschool.practice;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    public void should_return_1_given_number_1(){
+        //given
+
+        //when
+        String result = fizzBuzz.say(1);
+
+        //then
+        assertThat(result, is("1"));
+    }
+}
