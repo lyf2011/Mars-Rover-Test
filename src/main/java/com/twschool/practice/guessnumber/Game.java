14911462c042c114f0363e19ff165d37dce149dc
liyaofeng <932550363@qq.com>
2020-06-19 19:34:23 +0800

    [liyaofeng]当猜了6次后仍没猜对的情况，测试用例输出为FAILED

--- src/main/java/com/twschool/practice/guessnumber/Game.java
+++ src/main/java/com/twschool/practice/guessnumber/Game.java
@@ -1,17 +1,22 @@
 package com.twschool.practice.guessnumber;
 
 public class Game {
+    private static final int MAX_TRY_TIMES = 6;
     private GameAnswer answer;
     private GameStatus status = GameStatus.CONTINUE;
+    private int leftTimes = MAX_TRY_TIMES;
 
     public Game(GameAnswer answer) {
         this.answer = answer;
     }
 
     public String guess(String userInput) {
+        leftTimes--;
         String result = answer.check(userInput);
         if ("4A0B".equals(result)){
             status = GameStatus.SUCCESS;
+        } else if(leftTimes == 0){
+            status = GameStatus.FAILED;
         }
         return result;
     }
--- src/test/java/com/twschool/practice/guessnumber/GameTest.java
+++ src/test/java/com/twschool/practice/guessnumber/GameTest.java
@@ -36,4 +36,19 @@ public class GameTest {
 
         assertThat(status, is(GameStatus.CONTINUE));
     }
+
+    @Test
+    public void should_return_FAILED_with_user_input_wrong_6_times_given_answer_1234(){
+        Game game = new Game(answer);
+
+        game.guess("1,2,5,6");
+        game.guess("1,2,3,6");
+        game.guess("1,2,4,6");
+        game.guess("8,2,5,6");
+        game.guess("3,2,5,6");
+        game.guess("5,2,9,6");
+        GameStatus status = game.getStatus();
+
+        assertThat(status, is(GameStatus.FAILED));
+    }
 }
