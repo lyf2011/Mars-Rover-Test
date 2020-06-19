2912b0762ec24d319cf6d69a3d3b9eb57fdb195e
liyaofeng <932550363@qq.com>
2020-06-19 11:44:20 +0800

    [liyaofeng] 新建position类后，将所有测试用例都改成新的初始化方式

--- src/test/java/com/twschool/practice/MarsRoverTest.java
+++ src/test/java/com/twschool/practice/MarsRoverTest.java
@@ -11,7 +11,6 @@ public class MarsRoverTest {
     @Test
     public void should_return_0_0_W_given_0_0_N_and_L() {
         //given
-
         MarsRoverPostion position = new MarsRoverPostion(0, 0, "N");
         MarsRover marsRover = new MarsRover(position);
 
@@ -19,9 +18,6 @@ public class MarsRoverTest {
         marsRover.execute(Arrays.asList("L"));
 
         //then
-//        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-//        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-//        assertThat(marsRover.getDirection(), is("W"));
         assertThat(marsRover.getPosition().x, is(0));
         assertThat(marsRover.getPosition().y, is(0));
         assertThat(marsRover.getPosition().direction, is("W"));
@@ -30,192 +26,180 @@ public class MarsRoverTest {
     @Test
     public void should_return_0_0_E_given_0_0_N_and_R() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "N";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "N");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("R"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("E"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("E"));
     }
 
     @Test
     public void should_return_0_1_N_given_0_0_N_and_M() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "N";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "N");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("M"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(1));
-        assertThat(marsRover.getDirection(), is("N"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(1));
+        assertThat(marsRover.getPosition().direction, is("N"));
     }
 
     @Test
     public void should_return_0_0_S_given_0_0_W_and_L() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "W";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "W");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("L"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("S"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("S"));
     }
 
     @Test
     public void should_return_0_0_N_given_0_0_W_and_R() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "W";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "W");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("R"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("N"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("N"));
     }
 
     @Test
     public void should_return_negative_1_0_W_given_0_0_W_and_M() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "W";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "W");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("M"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(-1));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("W"));
+        assertThat(marsRover.getPosition().x, is(-1));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("W"));
     }
 
     @Test
     public void should_return_0_0_E_given_0_0_S_and_L() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "S";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "S");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("L"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("E"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("E"));
     }
 
     @Test
     public void should_return_0_0_W_given_0_0_S_and_R() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "S";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "S");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("R"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("W"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("W"));
     }
 
     @Test
     public void should_return_0_negative_1_S_given_0_0_S_and_M() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "S";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "S");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("M"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(-1));
-        assertThat(marsRover.getDirection(), is("S"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(-1));
+        assertThat(marsRover.getPosition().direction, is("S"));
     }
 
     @Test
     public void should_return_0_0_N_given_0_0_E_and_L() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "E";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "E");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("L"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("N"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("N"));
     }
 
     @Test
     public void should_return_0_0_S_given_0_0_E_and_R() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "E";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "E");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("R"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("S"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("S"));
     }
 
     @Test
     public void should_return_1_0_E_given_0_0_E_and_M() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "E";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "E");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("M"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(1));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(0));
-        assertThat(marsRover.getDirection(), is("E"));
+        assertThat(marsRover.getPosition().x, is(1));
+        assertThat(marsRover.getPosition().y, is(0));
+        assertThat(marsRover.getPosition().direction, is("E"));
     }
 
     @Test
     public void should_return_0_1_S_given_0_0_N_and_MMLLM() {
         //given
-        Coordinate coordinate = new Coordinate(0, 0);
-        String direction = "N";
-        MarsRover marsRover = new MarsRover(coordinate, direction);
+        MarsRoverPostion position = new MarsRoverPostion(0, 0, "N");
+        MarsRover marsRover = new MarsRover(position);
 
         //when
         marsRover.execute(Arrays.asList("M", "M", "L", "L", "M"));
 
         //then
-        assertThat(marsRover.getCoordinate().getCoordinateX(), is(0));
-        assertThat(marsRover.getCoordinate().getCoordinateY(), is(1));
-        assertThat(marsRover.getDirection(), is("S"));
+        assertThat(marsRover.getPosition().x, is(0));
+        assertThat(marsRover.getPosition().y, is(1));
+        assertThat(marsRover.getPosition().direction, is("S"));
     }
 }
