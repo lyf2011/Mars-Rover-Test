0eb235a8f7ea4d700cd15e11b823ba045a30c6c0
liyaofeng <932550363@qq.com>
2020-06-19 21:04:31 +0800

    [liyaofeng]测试产生的数字个数是可控的，比如给定4个或者5个，返回结果是对的

--- src/main/java/com/twschool/practice/guessnumber/Generater.java
+++ src/main/java/com/twschool/practice/guessnumber/Generater.java
@@ -1,14 +1,26 @@
 package com.twschool.practice.guessnumber;
 
+import java.util.Arrays;
+
 public class Generater {
     private int numberCount;
-    private String numberStrings;
+    private String numberStrings = "";
 
     public Generater(int numberCount) {
         this.numberCount = numberCount;
     }
 
     public String createNumerStrings() {
-        return "1,2,3,4";
+        String temStr = "";
+        for (int i = 0; i <numberCount ; i++) {
+            temStr += generateInt() + ",";
+        }
+        numberStrings = temStr.substring(0,temStr.length()-1);
+        return numberStrings;
+    }
+
+    private int generateInt(){
+        int random= (int)(Math.random()*10);
+        return random;
     }
 }
--- src/test/java/com/twschool/practice/guessnumber/GeneraterTest.java
+++ src/test/java/com/twschool/practice/guessnumber/GeneraterTest.java
@@ -2,6 +2,11 @@ package com.twschool.practice.guessnumber;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -16,5 +21,17 @@ public class GeneraterTest {
         int numbersLength = numberStrings.split(",").length;
         //then
         assertThat(numbersLength, is(4));
+    }
+
+    @Test
+    public void should_return_5_given_number_count_is_5(){
+        //given
+        int numberCount = 5;
+        //when
+        Generater generater = new Generater(numberCount);
+        String numberStrings = generater.createNumerStrings();
+        int numbersLength = numberStrings.split(",").length;
+        //then
+        assertThat(numbersLength, is(5));
     }
 }
